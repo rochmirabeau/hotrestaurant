79bc80c8c716617963f4a182a57bab86391ec9da
ivanleo94 <31164505+ivanleo94@users.noreply.github.com>
2018-02-03 11:23:48 -0700

    Update server.js

--- server.js
+++ server.js
@@ -23,3 +23,12 @@ app.get('/reserve', function (req, res) {
   console.log('reserve page requested');
   res.sendFile(path.join(__dirname, 'app/public/reserve.html'));
 });
+
+// reserve API call
+app.post('/api/reserve', function (req, res) {
+	console.log('reserve request submitted');
+	console.log(req.body);
+
+  var newReservation = req.body;
+
+  tables.push(newReservation);
