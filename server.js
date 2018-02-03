bb46b60ea1cd9ec9c2544d3146f6f3a010cdd61c
ivanleo94 <31164505+ivanleo94@users.noreply.github.com>
2018-02-03 11:05:51 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,3 +3,23 @@
 var express = require('express');
 var Path = require('path');
 var bodyParser= require('body-parser');
+
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get('/', function (req, res) {
+	console.log('home page requested');
+	res.sendFile(path.join(__dirname, 'app/public/index.html'));
+});
+
+app.get('/tables', function (req, res) {
+  console.log('tables page requested');
+  res.sendFile(path.join(__dirname, 'app/public/tables.html'));
+});
+
+app.get('/reserve', function (req, res) {
+  console.log('reserve page requested');
+  res.sendFile(path.join(__dirname, 'app/public/reserve.html'));
+});
